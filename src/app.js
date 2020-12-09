9e279e7d29b493e397e6ddb28a51a96fad21f34f
ARaquel1992 <73603870+ARaquel1992@users.noreply.github.com>
2020-12-09 16:20:55 +0000

    Added JS to current date

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-alert("hello");
+//Day and Time
+function formatDate (date) {
+    let weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let weekDay = weekDays[date.getDay()];
+    console.log(weekDays[date.getDay()]);
+
+    let months =["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let month = months[date.getMonth()];
+    console.log(months[date.getMonth()]);
+
+    let day = now.getDate();
+    return `${weekDay}, ${month}, ${day}`;
+}
+let todayDate = document.querySelector("#current-date");
+let now = new Date();
+console.log(new Date());
+todayDate.innerHTML = formatDate(now);
