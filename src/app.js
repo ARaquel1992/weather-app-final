1fe3b0392a6174448b0472354b1c6ad70c31455f
ARaquel1992 <73603870+ARaquel1992@users.noreply.github.com>
2020-12-10 04:20:40 +0000

    Added JS to the cuurent weather information

--- src/app.js
+++ src/app.js
@@ -36,7 +36,36 @@ currentHour.innerHTML = formatHours(now);
 
 //Request Forecast
 let apiKey = "f84d3c7abfdce95b297035c27acaaab5";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city name}&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then();
+axios.get(apiUrl).then(currentData);
 
+//Current Details (temp, wind)
+function currentData(response) {
+    console.log(response);
+    let temperature = document.querySelector("#today-temperature");
+    temperature.innerHTML = Math.round(response.data.main.temp);
+
+    let city = response.data.name;
+    let country = response.data.sys.country;
+    let h1 = document.querySelector("#searched-city");
+    h1.innerHTML = `${city}, ${country}`;
+
+    let feelsLike = document.querySelector("#feeling");
+    feelsLike.innerHTML = Math.round(response.data.main.feels_like);
+
+    let maxToday = document.querySelector("#max-temp-today");
+    maxToday.innerHTML = Math.round(response.data.main.temp_max);
+
+    let minToday = document.querySelector("#min-temp-today");
+    minToday.innerHTML = Math.round(response.data.main.temp_min);
+
+    let humidityToday = document.querySelector("#humidity");
+    humidityToday.innerHTML = response.data.main.humidity;
+
+    let wind = document.querySelector("#wind");
+    wind.innerHTML = response.data.wind.speed;
+
+    let description = document.querySelector("#description");
+    description.innerHTML = response.data.weather[0].description;
+}
